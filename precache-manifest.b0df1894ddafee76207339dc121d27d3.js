0b92c2f71a383c2489725bbcbba6df64feb16e71
Giovani Generali <giovani.generali@gmail.com>
2019-03-05 12:16:41 -0300

    Updates

---
+++ precache-manifest.b0df1894ddafee76207339dc121d27d3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/gitcompare/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/gitcompare/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/gitcompare/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/gitcompare/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/gitcompare/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "dd9035d1acb93032aa19",
+    "url": "/gitcompare/static/js/runtime~main.dd9035d1.js"
+  },
+  {
+    "revision": "26a4295ae89c07e4342a",
+    "url": "/gitcompare/static/js/main.26a4295a.chunk.js"
+  },
+  {
+    "revision": "2a0317afafce174b263d",
+    "url": "/gitcompare/static/js/2.2a0317af.chunk.js"
+  },
+  {
+    "revision": "2a0317afafce174b263d",
+    "url": "/gitcompare/static/css/2.e8defce6.chunk.css"
+  },
+  {
+    "revision": "97b064aaf56b3548c74e3fed121a51c3",
+    "url": "/gitcompare/index.html"
+  }
+];
